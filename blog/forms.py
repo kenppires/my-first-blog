ce59748c2d4cf71954b5ad68c6fa0e4a405b8e27
Kennedy <kenppires.icloud@gmail.com>
2026-01-12 22:33:17 -0300

    Adicionado view para criar/editar os post do blog dentro do site.

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
+        widgets = {
+            'text': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+        }
